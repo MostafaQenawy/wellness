fc302facc1c2c803fbcc7d7f224af4a1d85eb85f
Mohanad Khaled Hassan <mohanedkkhaled@gmail.com>
2025-05-15 04:38:03 +0300

    15/5/2025 4:37 Mohanad

--- src/main/java/com/graduation/wellness/model/dto/UserPlanWeekDayExerciseDTO.java
+++ src/main/java/com/graduation/wellness/model/dto/UserPlanWeekDayExerciseDTO.java
@@ -1,6 +1,6 @@
 package com.graduation.wellness.model.dto;
 
-public record UserPlanWeekDayExerciseDTO(Long id, String exerciseName, String description, String regioNMuscle
+public record UserPlanWeekDayExerciseDTO(Long id, String exerciseName, String description, String regionMuscle
         , String targetMuscle, String difficulty, int exerciseOrder, boolean exerciseDone, String equipmentType
         , String sets, String imageUrl, String videoUrl) {
 }

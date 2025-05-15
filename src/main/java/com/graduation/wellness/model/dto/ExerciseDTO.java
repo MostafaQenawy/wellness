220527ce07993cd073bfabf15157e44d43d6903f
Mohanad Khaled Hassan <mohanedkkhaled@gmail.com>
2025-05-15 04:32:40 +0300

    15/5/2025 4:32 Mohanad

--- src/main/java/com/graduation/wellness/model/dto/ExerciseDTO.java
+++ src/main/java/com/graduation/wellness/model/dto/ExerciseDTO.java
@@ -1,5 +1,5 @@
 package com.graduation.wellness.model.dto;
 
 public record ExerciseDTO(Long id, String exerciseName, String description, String regioNMuscle
-        , String targetMuscle, String difficulty, String sets, String imageUrl, String videoUrl) {
+        , String targetMuscle,String equipmentType, String difficulty, String sets, String imageUrl, String videoUrl) {
 }
--- src/main/java/com/graduation/wellness/model/dto/UserPlanWeekDayExerciseDTO.java
+++ src/main/java/com/graduation/wellness/model/dto/UserPlanWeekDayExerciseDTO.java
@@ -1,6 +1,6 @@
 package com.graduation.wellness.model.dto;
 
 public record UserPlanWeekDayExerciseDTO(Long id, String exerciseName, String description, String regioNMuscle
-        , String targetMuscle, String difficulty, int exerciseOrder, boolean exerciseDone
+        , String targetMuscle, String difficulty, int exerciseOrder, boolean exerciseDone, String equipmentType
         , String sets, String imageUrl, String videoUrl) {
 }
